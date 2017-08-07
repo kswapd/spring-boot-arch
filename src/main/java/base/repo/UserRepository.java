d1a32901532f77154cb5b43ec7b3662567bd4ca3
kongxiangwen <xw.kong@hnair.com>
2017-08-07 10:58:19 +0800

    add mysql support

---
+++ src/main/java/base/repo/UserRepository.java
@@ -0,0 +1,17 @@
+package base.repo;
+
+/**
+ * Created by kongxiangwen on 8/7/17.
+ */
+import base.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
